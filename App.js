5dd05e5364d56554f0bb5fbac9ba68395c375759
Rahal1999 <48178630+Rahal1999@users.noreply.github.com>
2019-05-06 00:45:23 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-
 import React, { Component } from 'react';
 
 import {
@@ -8,35 +7,33 @@ import {
    TouchableHighlight, 
    PermissionsAndroid, 
    Platform, 
-   StyleSheet
+   StyleSheet,
+   TouchableOpacity,
+   Alert
   } from 'react-native';
 
-
 import { CameraKitCameraScreen, } from 'react-native-camera-kit';
-
 
 export default class App extends Component {
   constructor() {
     super();
     this.state = {
-      //variable to hold the qr value
-      qrvalue: '',
+      qrvalue: '',        //variable to hold the qr value
       opneScanner: false,
     };
   }
+
   onOpenlink() {
-    //Function to open URL, If scanned 
     Linking.openURL(this.state.qrvalue);
-    //Linking used to open the URL in any browser that you have installed
   }
+  //after scanning qr code
   onBarcodeScan(qrvalue) {
-    //called after te successful scanning of QRCode/Barcode
     this.setState({ qrvalue: qrvalue });
     this.setState({ opneScanner: false });
   }
   onOpneScanner() {
     var that =this;
-    //To Start Scanning
+      
     if(Platform.OS === 'android'){
       async function requestCameraPermission() {
         try {
@@ -47,7 +44,7 @@ export default class App extends Component {
             }
           )
           if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-            //If CAMERA Permission is granted
+            
             that.setState({ qrvalue: '' });
             that.setState({ opneScanner: true });
           } else {
@@ -58,52 +55,79 @@ export default class App extends Component {
           console.warn(err);
         }
       }
-      //Calling the camera permission function
       requestCameraPermission();
     }else{
       that.setState({ qrvalue: '' });
       that.setState({ opneScanner: true });
     }    
   }
+
+  handlePress = async () => {
+    fetch('http://35.246.54.179/detectShop/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          "shop_name" :this.state.qrvalue,
+          "branch" : "Kadawatha"
+         
+        })
+  })
+      .then((response) => response.json())
+      .then((responseJson) => {
+      Alert.alert(responseJson.msg+" : "+this.state.qrvalue);
+   
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
   render() {
     let displayModal;
-    //If qrvalue is set then return this view
+    //If qrvalue is set 
     if (!this.state.opneScanner) {
       return (
         <View style={styles.container}>
-            <Text style={styles.heading}>React Native --- QR Code</Text>
-            <Text style={styles.simpleText}>{this.state.qrvalue ? 'Scanned QR Code: '+this.state.qrvalue : ''}</Text>
+            <Text style={styles.heading}> QR Code Scanner</Text>
+            {/* <Text style={styles.simpleText}>{this.state.qrvalue ? 'Scanned QR Code: '+this.state.qrvalue : ''}</Text>
             {this.state.qrvalue.includes("http") ? 
               <TouchableHighlight
                 onPress={() => this.onOpenlink()}
                 style={styles.button}>
-                  <Text style={{ color: '#FFFFFF', fontSize: 12 }}>Open Link</Text>
+                  <Text style={{ color: '#FFFFFF', fontSize: 13 }}>Open Link</Text>
               </TouchableHighlight>
               : null
-            }
+            } */}
             <TouchableHighlight
               onPress={() => this.onOpneScanner()}
               style={styles.button}>
-                <Text style={{ color: '#FFFFFF', fontSize: 12 }}>
-                Open QR Scanner
+                <Text style={{ color: '#FFFFFF', fontSize: 18 }}>
+                Touch to Scan
                 </Text>
             </TouchableHighlight>
+            
+
+            <TouchableOpacity onPress={this.handlePress.bind(this)}>
+             <Text style={{paddingTop: 50, color: '#FF0000',fontSize:20}}> ---Check if connected with the Shop--- </Text>
+            </TouchableOpacity>
+
         </View>
       );
     }
     return (
       <View style={{ flex: 1 }}>
         <CameraKitCameraScreen
-          showFrame={true}////////////////////////
-          //Show/hide scan frame
+          showFrame={true}
+          
           scanBarcode={true}
-          //Can restrict for the QR Code only
+          
           laserColor={'red'}
-          //Color can be of your choice
+          
           frameColor={'yellow'}
-          //If frame is visible then frame color
+          
           colorForScannerFrame={'black'}
-          //Scanner Frame color
+          
           onReadCode={event =>
             this.onBarcodeScan(event.nativeEvent.codeStringValue)
           }
@@ -120,7 +144,7 @@ const styles = StyleSheet.create({
     backgroundColor:'white'
   },
   button: {
-    alignItems: 'center',
+    alignItems: 'center', 
     backgroundColor: '#2c3539',
     padding: 10,
     width:300,
@@ -128,16 +152,10 @@ const styles = StyleSheet.create({
   },
   heading: { 
     color: 'black', 
-    fontSize: 24, 
+    fontSize: 35, 
     alignSelf: 'center', 
     padding: 10, 
     marginTop: 30 
   },
-  simpleText: { 
-    color: 'black', 
-    fontSize: 20, 
-    alignSelf: 'center', 
-    padding: 10, 
-    marginTop: 16
-  }
+  
 });
